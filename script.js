edc852f3d2665cf2f281b48849c6cb03bb06f9e6
Vanessa Gonçalves <98989640+vnessagm@users.noreply.github.com>
2022-07-13 09:02:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function validateFiels(){
 
    if(user.value.length>=1 && pass.value.length>=8){
        button.classList.add("changeColor")
-       button.setAttribute("disabled")
+       button.removeAttribute("disabled")
    } else{
     button.classList.remove("changeColor")
     button.setAttribute("disabled", "disabled")
